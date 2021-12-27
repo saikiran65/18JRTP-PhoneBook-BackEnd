cca2079c948522bb660454de8268e8eef913049e
nkirangoud <nkirangoud1@gmail.com>
2021-12-27 20:11:52 +0530

    cors config class added

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -2,11 +2,10 @@ package in.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 //@CrossOrigin(origins = "http://localhost:4200" )
-@CrossOrigin(origins = "https://aqueous-spire-28712.herokuapp.com")
+//@CrossOrigin(origins = "https://aqueous-spire-28712.herokuapp.com")
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/in/ashokit/configuration/corsConfig.java
@@ -0,0 +1,21 @@
+package in.ashokit.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class corsConfig {
+	
+	 @Bean
+	    public WebMvcConfigurer corsConfigurer() 
+	    {
+	        return new WebMvcConfigurer() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**").allowedOrigins("https://aqueous-spire-28712.herokuapp.com");
+	            }
+	        };
+	    }
+}
