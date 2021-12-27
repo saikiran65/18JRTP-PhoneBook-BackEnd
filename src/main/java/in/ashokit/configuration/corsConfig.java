ba014e9583a311e7f2bea89939a12376a12f7953
nkirangoud <nkirangoud1@gmail.com>
2021-12-27 21:07:14 +0530

    cors filter added in cors config class

--- src/main/java/in/ashokit/configuration/corsConfig.java
+++ src/main/java/in/ashokit/configuration/corsConfig.java
@@ -1,7 +1,11 @@
 package in.ashokit.configuration;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -9,15 +13,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class corsConfig {
 	
-	 @Bean
-	    public WebMvcConfigurer corsConfigurer() 
-	    {
-	        return new WebMvcConfigurerAdapter() {
-			
-	            @Override
-	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("/**").allowedOrigins("https://aqueous-spire-28712.herokuapp.com");
-	            }
-	        };
-	    }
+	/*
+	 * @Bean public WebMvcConfigurer corsConfigurer() { return new
+	 * WebMvcConfigurerAdapter() {
+	 * 
+	 * @Override public void addCorsMappings(CorsRegistry registry) {
+	 * registry.addMapping("/**").allowedOrigins(
+	 * "https://aqueous-spire-28712.herokuapp.com"); } }; }
+	 */
+	
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("http://domain1.com");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
+	}
 }
